173bedb2d76f4d601f49e785c8c037514506ef7e
Seyonce <seyonce@users.noreply.github.com>
2025-05-19 17:56:42 +0200

    13 test refractor

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const auth = require('../middlewares/auth');
 const { updateProfile, getCurrentUser } = require('../controllers/users');
 
 // GET /users/:userId - Get user by ID
